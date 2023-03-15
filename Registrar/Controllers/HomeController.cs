15823f13232bc356c351315289cc222e81863aef
Vera Weikel <vera.weikel@gmail.com>
2023-03-15 09:00:28 -0700

    Update home controller to intake courses/student in Dict[] for html display.

--- Registrar/Controllers/HomeController.cs
+++ Registrar/Controllers/HomeController.cs
@@ -7,9 +7,21 @@ namespace Registrar.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly RegistrarContext _db;
+
+    public HomeController(RegistrarContext db)
+    {
+      _db = db;
+    }
+
     [HttpGet("/")]
     public ActionResult Index()
     {
+      Course[] crss = _db.Courses.ToArray();
+      Student[] students = _db.Students.ToArray();
+      Dictionary<string, object[]> model = new Dictionary<string, object[]>();
+      model.Add("courses", crss);
+      model.Add("students", students);
       return View();
     }
   }
